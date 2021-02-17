d9d85968754a849a71c07695329e9d571c95164d
benzkittisak <6206021621082@fitm.kmutnb.ac.th>
2021-02-17 18:09:00 +0700

    week7_homework

---
+++ highestDigit.js
@@ -0,0 +1,21 @@
+const  highestDigit = (num) =>{
+    /*
+    let x = num.toString().split('');
+    let result = [];
+    for(let temp of x ){
+        result.push(parseInt(temp));
+    }
+    return Math.max(...result);*/
+
+    
+    let x = num.toString().split('');
+    let max = 0; 
+    for(let y of x){
+        if(max < y) max = y;
+    }
+    return parseInt(max);
+
+}
+console.log(highestDigit(379))
+console.log(highestDigit(2))
+console.log(highestDigit(377401))
